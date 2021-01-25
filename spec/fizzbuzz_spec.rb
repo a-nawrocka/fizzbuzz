e6167e5691f9cb9c39fbae3f3977b96282d7fe47
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-01-25 11:05:10 +0000

    FizzBuzz completed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,21 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  
+  it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  
+   it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(25)).to eq 'buzz'
+  end
+  
+    it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  
+   it 'returns number when passed not a  multiple of 5 or 3' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+  
 end
